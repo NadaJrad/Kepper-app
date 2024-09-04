c6b80d99d3e2558a9d561d3f2eebc174388377f4
NadaJrad <nadajradbenaicha@gmail.com>
2024-09-04 11:19:56 +0200

    update the keeper App Using arrow function and mapping

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "../my-app/component/Header";
-import Footer from "../my-app/component/Footer";
-import Note from "../my-app/component/Note";
-
-ReactDOM.render(
-  <div>
-    <Header />
-    <Footer />
-    <Note />
-  </div>,
-  document.getElementById("root")
-);
+import App from "../my-app/component/App";
+ReactDOM.render(<App />, document.getElementById("root"));
